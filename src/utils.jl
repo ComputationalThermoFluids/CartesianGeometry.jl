552d910ef190e21d1a9f47a2e2e9cc7e20f51fe7
Fastaxx <Fastaxx@github.com>
2024-10-22 10:42:35 +0200

    retrieve

--- src/utils.jl
+++ src/utils.jl
@@ -4,3 +4,14 @@ ispositive(x) = x > zero(x)
 isnegative(x) = x < zero(x)
 isnonnegative(x) = x ≥ zero(x)
 isnonpositive(x) = x ≤ zero(x)
+
+droplast(this::Base.OneTo) = Base.OneTo(length(this)-1)
+droplast(this::AbstractRange) = this[begin:end-1]
+
+dropends(this::AbstractRange) = this[begin+1:end-1]
+
+for T in [Float16, Float32, Float64]
+    @eval nan(::Type{$T}) = $T(NaN)
+    @eval nan(::Type{SVector{N,$T}}) where {N} =
+        SVector{N,$T}(ntuple(Returns(nan($T)), N))
+end
